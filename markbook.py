3c57af12192dccf41934bc2837958d4299ca9bdf
Felo Tadros <feloubatir.tadros21@ycdsbk12.ca>
2020-09-23 16:37:02 +0000

    5/6 functions commited

--- markbook.py
+++ markbook.py
@@ -6,15 +6,15 @@ from typing import Dict
 
 
 def create_assignment(name: str, due: str, points: int) -> Dict:
-  assignment = {"name": name, "due": due, "points": points}
-  return assignment	
+    assignment = {"name": name, "due": due, "points": points}
+    return assignment	
 
 
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
-  student_list = []
-	assignments_list = [] 
-  classroom = {"course_code": course_code, "course_name": course_name,"period": period, "teacher": teacher, "student_list": student_list, "assignments_list": assignments_list}
-  return classroom	
+    student_list = []
+    assignments_list = [] 
+    classroom = {"course_code": course_code, "course_name": course_name,"period": period, "teacher": teacher, "student_list": student_list, "assignment_list": assignments_list}
+    return classroom	
 
 def calculate_average_mark(student: Dict) -> float:
     """Calculates the average mark of a student"""
@@ -41,21 +41,28 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
 
 def remove_student_from_classroom(student: Dict, classroom: Dict):
     """Removes student from classroom
-
+  
     Args:
         student: The student to be removed
         classroom: the class from which the student will be removed.
+  
     """
+	student_list = classroom.get('student_list')
+	student_list.pop(student)
+	return student_list
+
     pass
 
 
 def edit_student(student: Dict, **kwargs: Dict):
+    
     """Edits the student's info with the provided key/value pairs
-
     Args:
         student: The student whose data needs to be updated.
         **kwargs: KeyWordARGumentS. The key/value pairs of the
             data that needs to be changed. Can come in the form
             of a dictionary.
     """
+
+    
     pass
