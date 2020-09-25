66cd773ba512cd9d4900f8c0a0fecf7c69cad99c
FTadros <43042263+FTadros@users.noreply.github.com>
2020-09-25 17:16:00 -0400

    Ensuring Pep8 standards are met

--- markbook.py
+++ markbook.py
@@ -5,17 +5,22 @@ Group members: Soham Faraz Felo
 from typing import Dict
 
 
-def create_assignment(name: str, due: str, points: int) -> Dict: #soham
+def create_assignment(name: str, due: str, points: int) -> Dict:  # soham
     assignment = {"name": name, "due": due, "points": points}
     return assignment
 
+
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     student_list = []
-    assignments_list = [] 
-    classroom = {"course_code": course_code, "course_name": course_name,"period": period, "teacher": teacher, "student_list": student_list, "assignment_list": assignments_list}
-    return classroom	
+    assignments_list = []
+    classroom = {"course_code": course_code, "course_name": course_name,
+                 "period": period, "teacher": teacher,
+                 "student_list": student_list,
+                 "assignment_list": assignments_list}
+    return classroom
 
-def calculate_average_mark(student: Dict) -> float: #felo
+
+def calculate_average_mark(student: Dict) -> float:  # felo
     """Calculates the average mark of a student"""
     student_grades = student.get('marks')
     total = 0
@@ -25,6 +30,7 @@ def calculate_average_mark(student: Dict) -> float: #felo
 
     avg = total / len(student_grades)
     return avg
+
 
 def add_student_to_classroom(student: Dict, classroom: Dict):
     """Adds student to a classroom
@@ -36,19 +42,19 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
     student_list = classroom.get('student_list')
     student_list.append(student)
 
-    return student_list   
+    return student_list
 
-def remove_student_from_classroom(student: Dict, classroom: Dict): #faraz
-	"""Removes student from classroom
 
-	Args:
-		student: The student to be removed
-		classroom: the class from which the student will be removed.
+def remove_student_from_classroom(student: Dict, classroom: Dict):  # faraz
+    '''Removes student from classroom
+    Args:
+    student: The student to be removed
+    classroom: the class from which the student will be removed.
+    '''
+    student_list = classroom.get('student_list')
+    student_list.remove(student)
+    return student_list
 
-	"""
-	student_list = classroom.get('student_list')
-	student_list.remove(student)
-	return student_list
 
 def edit_student(student: Dict, **kwargs: Dict):
     for key in kwargs.keys():
