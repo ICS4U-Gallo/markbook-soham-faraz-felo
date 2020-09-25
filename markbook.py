67a1967e61fb695945bfdd0d0c90cb8062a93cd8
Felo Tadros <feloubatir.tadros21@ycdsbk12.ca>
2020-09-25 20:55:18 +0000

    Adding json save file and comments

--- markbook.py
+++ markbook.py
@@ -5,10 +5,9 @@ Group members: Soham Faraz Felo
 from typing import Dict
 
 
-def create_assignment(name: str, due: str, points: int) -> Dict:
+def create_assignment(name: str, due: str, points: int) -> Dict: #soham
     assignment = {"name": name, "due": due, "points": points}
     return assignment
-
 
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     student_list = []
@@ -16,7 +15,7 @@ def create_classroom(course_code: str, course_name: str, period: int, teacher: s
     classroom = {"course_code": course_code, "course_name": course_name,"period": period, "teacher": teacher, "student_list": student_list, "assignment_list": assignments_list}
     return classroom	
 
-def calculate_average_mark(student: Dict) -> float:
+def calculate_average_mark(student: Dict) -> float: #felo
     """Calculates the average mark of a student"""
     student_grades = student.get('marks')
     total = 0
@@ -39,7 +38,7 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
 
     return student_list   
 
-def remove_student_from_classroom(student: Dict, classroom: Dict):
+def remove_student_from_classroom(student: Dict, classroom: Dict): #faraz
 	"""Removes student from classroom
 
 	Args:
@@ -48,19 +47,10 @@ def remove_student_from_classroom(student: Dict, classroom: Dict):
 
 	"""
 	student_list = classroom.get('student_list')
-	student_list.pop(student)
+	student_list.remove(student)
 	return student_list
 
-
 def edit_student(student: Dict, **kwargs: Dict):
-
-	"""Edits the student's info with the provided key/value pairs
-	Args:
-		student: The student whose data needs to be updated.
-		**kwargs: KeyWordARGumentS. The key/value pairs of the
-			data that needs to be changed. Can come in the form
-			of a dictionary.
-	"""
-	for key in kwargs.keys():
-		student[key] = kwargs[key]
-	return(student)
+    for key in kwargs.keys():
+        student[key] = kwargs[key]
+    return(student)
